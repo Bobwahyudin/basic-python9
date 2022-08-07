37450dc75f92aa07616c212fe264adea15af89c2
bob wahyudin <bobwahyudin@gmail.com>
2022-08-07 14:45:12 +0800

    tugas1

---
+++ meeting1/soal-1.py
@@ -0,0 +1,5 @@
+Nama = input ("Please enter your name: ")
+Umur = int (input("please enter your age in years: ")) 
+Tinggi = float (input("enter your height in centimeter"))
+
+print (f"Nama saya {Nama}, umur saya {Umur} tahun dan tinggi saya {Tinggi} cm.")
